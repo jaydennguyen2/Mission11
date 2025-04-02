5baf59fff9cbc1a779301f6551226a4c27765713
jaydennguyen815 <jaydennguyen0815@gmail.com>
2025-04-01 20:22:11 -0600

    fixed typo in getcategory link

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter ({
     useEffect (() => {
         const fetchCategories = async () => {
             try {
-            const response = await fetch('https://mission13-nguyen-backend.azurewebsites.net/Books/GetCategories');
+            const response = await fetch('https://mission13-nguyen-backend.azurewebsites.net/Book/GetCategories');
             const data = await response.json();
             console.log('Fetched categories: {data}');
             setCategories(data);
