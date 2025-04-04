3df363b6fea2564bf2784ad42ad20dcaa628b060
jaydennguyen815 <jaydennguyen0815@gmail.com>
2025-04-04 12:25:47 -0600

    category test

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter ({
     useEffect (() => {
         const fetchCategories = async () => {
             try {
-            const response = await fetch('https://mission13-nguyen-backend.azurewebsites.net/Book/getbookcategories');
+            const response = await fetch('');
             const data = await response.json();
             console.log('Fetched categories: {data}');
             setCategories(data);
